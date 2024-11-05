cc776fd1ec9401ef5c18683b9a846cf2cab93839
Louis Marquis <louiswmarquis@gmail.com>
2024-11-05 02:02:41 -0500

    try this

--- server/index.js
+++ server/index.js
@@ -129,10 +129,10 @@ app.listen(process.env.PORT || PORT || 5000, () => {
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('app/client/build'));
+  app.use(express.static(__dirname.substr(-7) + '/client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   app.get('/', (req, res) => {
-    res.sendFile("app/client/build/index.html")
+    res.sendFile(__dirname.substr(-7) + "/client/build/index.html")
   })
 }
