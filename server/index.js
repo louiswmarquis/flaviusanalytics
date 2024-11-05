ebefcbd846bdd1ecb3c9aa5c1e872bd781dabfb2
Louis Marquis <louiswmarquis@gmail.com>
2024-11-05 01:39:39 -0500

    forgot a slash

--- server/index.js
+++ server/index.js
@@ -133,6 +133,6 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express serve up index.html file if it doesn't recognize route
   app.get('*', (req, res) => {
-    res.sendFile(__dirname + "../client/build/index.html")
+    res.sendFile(__dirname + "/../client/build/index.html")
   })
 }
