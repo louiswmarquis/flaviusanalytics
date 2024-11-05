227133a99cf6b289adafbf5a2c6ceff476e0335d
Louis Marquis <louiswmarquis@gmail.com>
2024-11-05 08:27:39 -0500

    debug az-1

--- server/fetch.js
+++ server/fetch.js
@@ -135,9 +135,17 @@ function fetch_ddhq(url) {
                     fips = "02000"
                 }
                 results[fips] = {}
+                try {
                 for (const candidate_data of county_data["candidates"]) {
                     results[fips][candidates[candidate_data["cand_id"]]] = candidate_data["votes"]
                 }
+            }
+            catch (error) {
+                console.log(county_data)
+                console.log(county_data["candidates"])
+                console.log(data)
+                throw error
+            }
                 results[fips]["total"] = county_data["total_votes"]
                 results[fips]["turnout"] = county_data["estimated_votes"]["estimated_votes_mid"]
             }
