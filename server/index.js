98f5e92d24bc5b41487b59a5df8b3dc379e39257
Louis Marquis <louiswmarquis@gmail.com>
2024-11-05 01:48:38 -0500

    use env

--- server/index.js
+++ server/index.js
@@ -123,7 +123,7 @@ setInterval(() => {
   fetch_data();
 }, 10000);
 
-app.listen(PORT || 5000, () => {
+app.listen(process.env.PORT || PORT || 5000, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
